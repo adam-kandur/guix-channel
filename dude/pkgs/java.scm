d21eb7885f71ffd67d227d8e1e25f775b23dce6d
Adam <>
2023-03-14 15:20:58 +0300

    wip

---
+++ dude/pkgs/java.scm
@@ -0,0 +1,30 @@
+(define-module (dude pkgs java)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix packages)
+  #:use-module (guix packages java)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system java))
+
+(define-public java-hello-world
+  (let* ((revision "0")
+         (commit "f345bc49d7cc90f4599b7c4843755b4757f05739"))
+    (package
+      (name "java-hello-world")
+      (version (git-version "0.1" revision commit))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                      (url "https://github.com/adam-kandur/java-hello-world")
+                      (commit commit)))
+                (file-name (git-file-name name version))
+                (sha256
+                 (base32
+                  "1hhx8gmn81k8qrkx4p7ppinmygxga9fqffd626wkvhjgg2ky8lhs"))))
+      (build-system ant-build-system)
+      (arguments
+       `(#:jdk ,openjdk11
+         #:jar-name "swt.jar"))
+      (home-page "")
+      (synopsis "")
+      (description "")
+      (license #f))))
