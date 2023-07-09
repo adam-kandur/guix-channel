81506bdcfba38362be6ae83d66738835d32f28a7
Adam <>
2023-07-09 13:13:40 +0300

    wip

--- channel/dude/pkgs/lisp.scm
+++ channel/dude/pkgs/lisp.scm
@@ -10,14 +10,14 @@
 (define-public lem
   (package
     (name "lem")
-    (version "v2.0.0")
+    (version "2.0.0")
     (source
      (origin
        (method git-fetch)
        (uri (git-reference
              (url "https://github.com/lem-project/lem")
-             (commit version)))
-       (file-name (git-file-name "lem" version))
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
        (sha256
         (base32
          "1hwgl43mjwzvbcamdiqs8jv8961mp3hiar15cjcz3xwc5bdqwvi2"))))
@@ -38,3 +38,37 @@
     (description "")
     (home-page "")
     (license #f)))
+
+(define-public sbcl-inquisitor
+  (let ((revision "0")
+        (commit "423fa9bdd4a68a6ae517b18406d81491409ccae8"))
+    (package
+      (name "sbcl-inquisitor")
+      (version (git-version "0.1" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/t-sin/inquisitor")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32
+           "1hwgl43mjwzvbcamdiqs8jv8961mp3hiar15cjcz3xwc5bdqwvi2"))))
+      (build-system asdf-build-system/sbcl)
+      (arguments
+       `(#:asd-systems '("inquisitor")))
+      ;; (inputs
+      ;;  (list sbcl-alexandria
+      ;;        sbcl-trivial-gray-streams
+      ;;        sbcl-trivial-types
+      ;;        sbcl-trivial-types
+      ;;        sbcl-cl-ppcre))
+      ;; (inputs
+      ;;  (list sbcl-alexandria sbcl-anaphora))
+      ;; (native-inputs
+      ;;  (list sbcl-lift))
+      (synopsis "")
+      (description "")
+      (home-page "")
+      (license #f))))
